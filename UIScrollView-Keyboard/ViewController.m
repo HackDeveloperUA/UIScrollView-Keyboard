7fe24b9b918c3e37bfe6202b83a1e52ee5310e16
M1A1rx <thisismymail03@gmail.com>
2017-07-27 20:13:03 +0200

    first commit

--- UIScrollView-Keyboard/ViewController.m
+++ UIScrollView-Keyboard/ViewController.m
@@ -9,21 +9,103 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // 1. Регистрируем тап, для скрытия клавиатуры
+    // 1. Register tap to hide the keyboard
+    
+    UITapGestureRecognizer* tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapView:)];
+    [self.view addGestureRecognizer:tapGesture];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewWillAppear:(BOOL)animated
+{
+    // 2. Подписываемся на нотификации
+    // 2. Subscribe to notifications
+    
+    [super viewWillAppear:animated];
+    [self addObservers];
 }
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    // 3. Отписываемся от нотификаций
+    // 3. Unsubscribe from notifications
+    
+    [super viewWillDisappear:animated];
+    [self removeObservers];
+}
+
+#pragma mark - Keybords methods
+
+- (void) addObservers
+{
+    // Нотификация которая появляется при открытии клавиатуры
+    // Notification that appears when you open the keyboard
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:nil usingBlock:^(NSNotification*  note) {
+        [self keyboardWillShow:note];
+    }];
+    
+    // Нотификация которая появляется при закрытии клавиатуры
+    // Notification that appears when you close the keyboard
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:nil usingBlock:^(NSNotification*  note) {
+        [self keyboardWillHide:note];
+    }];
+}
+
+
+- (void) keyboardWillShow:(NSNotification*) notification
+{
+    // Получаем словарь - Get Dictionary
+    
+    NSDictionary* userInfo = notification.userInfo;
+    
+    if (userInfo)
+    {
+        // Вытаскиваем frame который описывает кооридинаты клавиатуры
+        // Pull out frame which describes the coordinates of the keyboard
+        CGRect frame = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+        
+        // Создаем отступ по высоте клавиатуры
+        // Create an inset at the height of the keyboard
+        
+        UIEdgeInsets contentInset = UIEdgeInsetsMake(0, 0, CGRectGetHeight(frame), 0);
+        
+        // Применяем отступ - Apply the inset
+        self.scrollView.contentInset = contentInset;
+    }
+}
+
+- (void) keyboardWillHide:(NSNotification*) notification
+{
+    // Отменяем отступ - Cancel inset
+    self.scrollView.contentInset = UIEdgeInsetsZero;
+}
+
+
+- (void) removeObservers
+{
+    // Отписываемся от нотификаций - Unsubscribe from notifications
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark -  Action
+
+- (void) didTapView:(UITapGestureRecognizer*) gesture {
+    [self.view endEditing:YES];
+}
+
 
 
 @end
